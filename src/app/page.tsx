8bcbb807eea3beec89ab84559d6dc22c826ff378
frizer congelado <frizerzin@gmail.com>
2025-09-23 03:38:50 +0000

    deixe o video do jeito certo e implemente o embed em javascript <script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -510,3 +510,4 @@ export default function Home() {
     </div>
   );
 }
+
--- src/components/WistiaPlayer.tsx
+++ src/components/WistiaPlayer.tsx
@@ -2,21 +2,45 @@
 
 import Script from 'next/script';
 
-declare global {
-  interface Window {
-    _wq: any[];
-  }
-}
-
 export function WistiaPlayer() {
   return (
     <>
       <Script src="https://fast.wistia.com/player.js" strategy="lazyOnload" />
-      <div className="wistia_embed wistia_async_8cbxka7tns videoFoam=true" style={{ height: '80vh', maxHeight: '720px', width: 'auto', aspectRatio: '0.5625' }}>
-        <div className="wistia_swatch" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%' }}>
-          <img src="https://fast.wistia.com/embed/medias/8cbxka7tns/swatch" style={{ filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%' }} alt="" aria-hidden="true" />
+      <div
+        className="wistia_embed wistia_async_8cbxka7tns videoFoam=true"
+        style={{
+          height: '100%',
+          position: 'relative',
+          width: '100%',
+        }}
+      >
+        <div
+          className="wistia_swatch"
+          style={{
+            height: '100%',
+            left: 0,
+            opacity: 0,
+            overflow: 'hidden',
+            position: 'absolute',
+            top: 0,
+            transition: 'opacity 200ms',
+            width: '100%',
+          }}
+        >
+          <img
+            src="https://fast.wistia.com/embed/medias/8cbxka7tns/swatch"
+            style={{
+              filter: 'blur(5px)',
+              height: '100%',
+              objectFit: 'contain',
+              width: '100%',
+            }}
+            alt=""
+            aria-hidden="true"
+          />
         </div>
       </div>
     </>
   );
 }
+
