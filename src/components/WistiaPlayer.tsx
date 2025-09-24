16447e694b59d5a339e97d3b80eca4a6ebae27e1
frizer congelado <frizerzin@gmail.com>
2025-09-24 18:38:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/WistiaPlayer.tsx
+++ src/components/WistiaPlayer.tsx
@@ -7,22 +7,13 @@ export function WistiaPlayer() {
   return (
     <>
       <div
+        className="wistia_embed wistia_async_8cbxka7tns"
         style={{
           height: '100%',
           position: 'relative',
           width: '100%',
         }}
-      >
-        <div
-          className="wistia_embed wistia_async_8cbxka7tns"
-          style={{
-            height: '100%',
-            position: 'relative',
-            width: '100%',
-          }}
-        >&nbsp;</div>
-      </div>
-      <Script src="https://fast.wistia.com/embed/medias/8cbxka7tns.jsonp" strategy="lazyOnload" />
+      >&nbsp;</div>
       <Script src="https://fast.wistia.com/assets/external/E-v1.js" strategy="lazyOnload" />
     </>
   );
