9164af45232da261f75e110a064f7e15436f4355
frizer congelado <frizerzin@gmail.com>
2025-09-26 07:14:22 +0000

    coloque esse pixel aqui no site <!-- Meta Pixel Code --> <script> !funct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Poppins, Lilita_One } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -40,6 +41,31 @@ export default function RootLayout({
       className={`${poppins.variable} ${lilitaOne.variable}`}
     >
       <body className="font-body antialiased">
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel-script" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '817922931179070');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=817922931179070&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+        
         {children}
         <Toaster />
       </body>
